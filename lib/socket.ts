43f97e58f92b5e7eee9a6972acb8611c24cc5caa
natthawut jampasaeng <121080181+Natthawut-jps@users.noreply.github.com>
2026-03-08 23:33:05 +0700

    socket

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client"
 
-export const socket = io({
+export const socket = io("https://agnos-assignment-production.up.railway.app",{
    transports: ["websocket"]
 })
